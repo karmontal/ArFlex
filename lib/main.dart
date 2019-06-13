f9de85d66f5ddabd866ce8e9d61b527b655c2892
KTALLA <karmontal86@gmail.com>
2019-06-13 08:10:37 +0300

    Fisrt with Ads

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:facebook_audience_network/facebook_audience_network.dart';
 
 //------ Global Variables------
 Movie selectedMovie;
@@ -32,6 +33,7 @@ _launchURL() async {
 //-----------------------------
 
 void main() async {
+  FacebookAudienceNetwork.init();
   runApp(new MyApp());
 }
 
@@ -86,7 +88,21 @@ class _MyListScreenState extends State {
       itemCount: latestMovies.length,
       separatorBuilder: (context, index) {
         if ((index + 1) % (5) == 0 && index > 0) {
-          return Divider();
+          return FacebookNativeAd(
+            placementId: "333970483937433_333971380604010",
+            adType: NativeAdType.NATIVE_BANNER_AD,
+            bannerAdSize: NativeBannerAdSize.HEIGHT_100,
+            width: double.infinity,
+            backgroundColor: Colors.blue,
+            titleColor: Colors.white,
+            descriptionColor: Colors.white,
+            buttonColor: Colors.deepPurple,
+            buttonTitleColor: Colors.white,
+            buttonBorderColor: Colors.white,
+            listener: (result, value) {
+              print("Native Ad: $result --> $value");
+            },
+          );
         } else
           return SizedBox(
             height: 0,
